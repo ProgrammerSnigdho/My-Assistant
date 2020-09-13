021dd99386db93537d526819827698dee0420455
ProgrammerSnigdho <programmersnigdho@gmail.com>
2020-09-13 14:48:34 +0600

    first commit

--- app.js
+++ app.js
@@ -99,18 +99,18 @@ function readOutLoud(message) {
         speech.text = "I know Siri";
     } else if (message.includes('can you code')) {
         speech.text = "I can't code 😭";
-    } else if (message.includes('owner of Blogger' || 'owner of blogger')) {
+    } else if (message.includes('owner of blogger')) {
         speech.text = "The Owner Of Blogger Is Google";
-    } else if (message.includes('owner of GitHub' || 'owner of Github' || 'owner of gitHub' || 'owner of github')) {
+    } else if (message.includes('owner of github')) {
         speech.text = "The Owner Of Github Is Microsoft";
-    } else if (message.includes('owner of Stack Overflow' || 'owner of Stack overflow' || 'owner of stack Overflow' || 'owner of stack overflow')) {
+    } else if (message.includes('owner of stack overflow')) {
         speech.text = "The Owner Of Stack Overflow Is Stack Exchange Incorporate";
     }
 
 
 
 
-    speech.volume = 1;
+    speech.volume = 2;
     speech.rate = 1;
     speech.pitch = 1;
 
